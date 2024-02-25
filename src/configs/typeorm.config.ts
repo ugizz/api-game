3f11f618396309ea237254369c4d2cdcdef0aa31
suwon <ysw7939@gmail.com>
2024-02-25 22:17:39 +0900

    ♻️refacotr: 24.02.25 양수원 컨피그 파일 수정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,7 +4,7 @@ import * as config from 'config';
 const dbConfig = config.get('db');
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: process.env.RDS_TYPE || dbConfig.type,
-  host: 'host.docker.internal' || process.env.RDS_HOSTNAME || dbConfig.host,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
   port: process.env.RDS_PORT || dbConfig.port,
   username: process.env.RDS_USERNAME || dbConfig.username,
   password: process.env.RDS_PASSWORD || dbConfig.password,
